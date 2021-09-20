563e82c8f5c35d10a3910532dfe9fbb0394de3c8
srinadh1304 <srinadh1304@gmail.com>
2021-09-20 23:53:33 +0530

    ADDED: Class to store user information

---
+++ lib/src/main/java/com/bridgelabz/opencsvandgson/CSVUser.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.opencsvandgson;
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVUser {
+	@CsvBindByName
+	private String name;
+	
+	@CsvBindByName(column="email", required=true)
+	private String email;
+	
+	@CsvBindByName(column="phone")
+	private String phoneNo;
+	
+	@CsvBindByName
+	private String country;
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	
+	public String getCountry() {
+		return country;
+	}
+
+	@Override
+	public String toString() {
+		return "POJOClass [name=" + name + ", email=" + email + ", phoneNo=" + phoneNo + ", country=" + country + "]";
+	}
+}
